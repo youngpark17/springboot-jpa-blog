3e5aaa33bf332fdcd6c2ed9b3ce80ff8a8c22eae
youngwoon <mduddns@naver.com>
2020-10-01 12:59:14 +0900

    환경 세팅 완료

---
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -0,0 +1,15 @@
+package com.cos.blog.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogControllerTest {
+
+	@GetMapping("/test/hello")
+	public String home() {
+		
+		return "<h1>blog/home<h1>";
+	}
+	
+}
